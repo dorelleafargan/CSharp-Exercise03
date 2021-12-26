0c4c4b56505d4e571d135c57259443b4cd294bf0
Dorelle Afargan <57329604+dorelleafargan@users.noreply.github.com>
2021-12-26 22:59:15 +0200

    Second Commit

--- A22 Ex03 Dorelle 204005235 Lior 316016476/Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ A22 Ex03 Dorelle 204005235 Lior 316016476/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -4,9 +4,14 @@ namespace Ex03.GarageLogic
 {
     public class ValueOutOfRangeException : Exception
     {
+        private readonly float r_MaxValue;
+        private readonly float r_MinValue;
+
         public ValueOutOfRangeException(float i_MinValue, float i_MaxValue)
-            : base(string.Format("Float entered exceeded the limits of the range {0}-{1}", i_MinValue, i_MaxValue))
+    : base(string.Format("Float entered exceeded the limits of the range {0}-{1}", i_MinValue, i_MaxValue))
         {
+            r_MinValue = i_MinValue;
+            r_MaxValue = i_MaxValue;
         }
     }
 }
